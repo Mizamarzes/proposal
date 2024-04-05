29af8daf2e978a52f7f924d816e7b3907cf46b64
mizamarzes <mizamarzes@gmail.com>
2024-04-04 23:57:26 -0500

    a

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert("sabia que ibas a decir que si, bartolo")
+    alert("Hola jen o siiiii")
 });
 
 const noBtn = document.querySelector('#noBtn');
