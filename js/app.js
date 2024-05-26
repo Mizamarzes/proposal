0c2ea5df245fd8de1629b0bb7500c4f61823b0e0
mizamarzes <mizamarzes@gmail.com>
2024-05-26 09:04:40 -0500

    mayra.2

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert("Vivan las papas locas")
-});
+    // alert("Vivan las papas locas")
+    Swal.fire({
+        title: "Hola amor, vivan las papas locas",
+        width: 500,
+        padding: "2em",
+        color: "#716add",
+        background: "rgba(207, 208, 218, 1)",
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `
+    })
+})
 
 const noBtn = document.querySelector('#noBtn');
 
